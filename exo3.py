62513790371ac216c6d802075f01fdccc6f61b18
sam-codeers <122085361+sam-codeers@users.noreply.github.com>
2023-02-06 09:43:01 +0000

    exo3.py

--- exo3.py
+++ exo3.py
@@ -1,9 +1,17 @@
-def fill_prefix(registration):
-    prefix = registration.split("-")[0]
-    if len(prefix) < 2:
-        prefix = prefix + " " * (2 - len(prefix))
-    return prefix
+def validate_plate(plate):
+    if len(plate) != 7:
+        return False
+    if not plate[0:2].isdigit():
+        return False
+    if not plate[3:7].isalpha():
+        return False
+    if plate[2] != '-':
+        return False
+    return True
 
-registration = input("Entrez l'immatriculation de l'avion : ")
-prefix = fill_prefix(registration)
-print("Préfixe :", prefix)
+plate = input("Entrez une plaque d'immatriculation sous la forme AA-BBBB : ")
+if validate_plate(plate):
+    print("Plaque valide")
+else:
+    print("Plaque non valide")
+
